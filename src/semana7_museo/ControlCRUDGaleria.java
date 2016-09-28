3d3118c305927cfdff00507927d816ef0d7a17f3
Genskoria <cur_informat@cur_lab0407.inacap.cl>
2016-09-28 10:30:34 -0300

    -jop n' clear button -ArrayList usuarios -Ver button + add method -no admin instance -null exceptions -class CCrudGaleria

---
+++ src/semana7_museo/ControlCRUDGaleria.java
@@ -0,0 +1,7 @@
+package semana7_museo;
+public class ControlCRUDGaleria {
+    public void IngresarNuevaGaleria(){}
+    public void EliminarGaleria(){}
+    public void ModificarGaleria(){}
+    public void VerGaleria(){}    
+}
